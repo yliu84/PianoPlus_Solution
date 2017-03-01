660b4f5d3199d00d785230f63db2e55bf09f2697
yliu84 <yliu84@studentmail.nait.ca>
2017-03-01 10:03:05 -0700

    Updated Login function on WebMaster Pager

--- PianoPlus_Website/WebMaster.master.cs
+++ PianoPlus_Website/WebMaster.master.cs
@@ -9,6 +9,25 @@ public partial class WebMaster : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Session["email"] != null)
+        {
+            a_email.InnerText = Session["email"].ToString();
+            li_loggedin.Visible = true;
+            li_signUp.Visible = false;
+            li_signIn.Visible = false;
+        }
+        else
+        {
+            li_loggedin.Visible = false;
+        }
 
+    }
+    protected void btn_logout_Click(object sender, EventArgs e)
+    {
+        Application.Lock();
+        ((List<string>)Application["Loggedin_Users"]).Remove(Session["email"].ToString());
+        Application.UnLock();
+        Session["email"] = null;
+        Response.Redirect("~/Default.aspx");
     }
 }
