cbeed9507d887537211eddad991c0dffa023b299
yliu84 <yliu84@studentmail.nait.ca>
2017-03-20 16:51:46 -0600

    Updated Create blog page

---
+++ PianoPlus_System/BLL/BlogController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PianoPlus_Data.Entities;
+using PianoPlus_Data;
+using PianoPlus_Data.POCOS;
+using System.ComponentModel;
+using System.Data.Entity;
+
+namespace PianoPlus_System.BLL
+{
+    public class BlogController
+    {
+        public bool AddPost(Blog newBlog)
+        {
+            
+      
+                using (var context = new PianoPlusContext())
+                {
+                    context.Blogs.Add(newBlog);
+
+                    context.SaveChanges();
+                    return true;
+                    
+                }
+           
+            
+        }
+    }
+}
--- PianoPlus_Website/CreatePost.aspx.cs
+++ PianoPlus_Website/CreatePost.aspx.cs
@@ -5,10 +5,65 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using PianoPlus_Data.Entities;
+using PianoPlus_System.BLL;
+
 public partial class CreatePost : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+    protected void btn_add_Click(object sender, EventArgs e)
+    {
+        if(Page.IsValid)
+        {
+            if (Session["email"] != null && Session["RoleID"].ToString().Equals("Adm"))
+            {
+
+                string title = txt_title.Text;
+                string postText = txt_text.Text;
+                string instructorID = Session["InstructorID"].ToString();
+                BlogController controller = new BlogController();
+
+
+
+            //    MessageUserControl.TryRun(() =>
+            //{
+            //    Blog newPost = new Blog();
+
+            //    newPost.Title = title;
+            //    newPost.InstructorID = int.Parse(instructorID);
+            //    newPost.Content = postText;
+            //    newPost.PostDate = DateTime.Now;
+
+            //    controller.AddPost(newPost);
+            //}, "Success", "New bolg post added.");
+                Blog newPost = new Blog();
+
+                newPost.Title = title;
+                newPost.InstructorID = int.Parse(instructorID);
+                newPost.Content = postText;
+                newPost.PostDate = DateTime.Now;
+                if (controller.AddPost(newPost))
+                {
+                    MessageUserControl.ShowInfo("Add new blog post successful");
+                }
+                else
+                {
+                    MessageUserControl.ShowInfo("Cannot add new student, please try again.");
+                }
+
+
+            }
+            else
+            {
+                Response.Redirect("~/Login.aspx");
+            }
+
+        
+            
+
+        }
     }
 }
