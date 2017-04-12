112080bf8828ab09283bdb2297c6a3a4389da5bb
yliu84 <yliu84@studentmail.nait.ca>
2017-04-12 11:59:26 -0600

    added signalR and chatroom

---
+++ PianoPlus_Website/App_Code/MessageDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for MessageDetail
+/// </summary>
+public class MessageDetail
+{
+	public MessageDetail()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public string UserName { get; set; }
+
+    public string Message { get; set; }
+}
---
+++ PianoPlus_Website/App_Code/UserDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for UserDetail
+/// </summary>
+public class UserDetail
+{
+	public UserDetail()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+        
+	}
+    public string ConnectionId { get; set; }
+    public string UserName { get; set; }
+
+}
